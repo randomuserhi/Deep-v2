effb4d785c7046ac94d7acff126acdecd2e2e4df
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2026-07-18 23:56:53 +0100

    Update IntegerBitMask.h

--- Deep/public/Deep/Bit/IntegerBitMask.h
+++ Deep/public/Deep/Bit/IntegerBitMask.h
@@ -9,6 +9,7 @@
 DEEP_NAMESPACE_BEGIN
 
 // Describes the shape required for a bitmask type.
+// Allows for both fixed length and dynamic length bit masks.
 template<typename T>
 concept c_BitMask = std::copyable<T> && requires(T a, const T ca, std::size_t i, bool v) {
 	{ T::k_maxNumBits } -> std::convertible_to<size_t>;
