67ecef1b383bd5f61b46d41a197dc3d1e9a90e13
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2026-08-11 03:39:49 +0100

    Add Storage Policy

--- Deep/include/public_cpp/Deep/Bit/BitMask.h
+++ Deep/include/public_cpp/Deep/Bit/BitMask.h
@@ -7,23 +7,29 @@
 
 DEEP_NAMESPACE_BEGIN
 
-template<size_t T>
+// TODO(randomuserhi): Move away from std::bitset for performance of IndexOfLowestSetBit.
+//                     Also allows implementation of a storage policy if bitset is too large for the stack.
+//                     (Uses a std::array<Chunk, in_N> instead of a Chunk*):
+//                     template<size_t in_N, StoragePolicy in_StoragePolicy>
+//                     - StoragePolicy::e_stack, StoragePolicy::e_heap
+
+template<size_t in_N>
 class BitMask;
 
-template<size_t T>
-using Arg_BitMask = const BitMask<T>;
+template<size_t in_N>
+using Arg_BitMask = const BitMask<in_N>;
 
-template<size_t T>
-constexpr Deep_ForceInline BitMask<T> operator|(BitMask<T>, Arg_BitMask<T>);
+template<size_t in_N>
+constexpr Deep_ForceInline BitMask<in_N> operator|(BitMask<in_N>, Arg_BitMask<in_N>);
 
-template<size_t T>
-constexpr Deep_ForceInline BitMask<T> operator&(BitMask<T>, Arg_BitMask<T>);
+template<size_t in_N>
+constexpr Deep_ForceInline BitMask<in_N> operator&(BitMask<in_N>, Arg_BitMask<in_N>);
 
-template<size_t T>
-constexpr Deep_ForceInline BitMask<T> operator~(BitMask<T>);
+template<size_t in_N>
+constexpr Deep_ForceInline BitMask<in_N> operator~(BitMask<in_N>);
 
-template<size_t T>
-constexpr Deep_ForceInline bool operator==(Arg_BitMask<T>, Arg_BitMask<T>);
+template<size_t in_N>
+constexpr Deep_ForceInline bool operator==(Arg_BitMask<in_N>, Arg_BitMask<in_N>);
 
 // Bitmask wrapper for std::bitset
 template<size_t in_N>
---
+++ Deep/include/public_cpp/Deep/Memory/StoragePolicy.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Deep.h"
+
+DEEP_NAMESPACE_BEGIN
+
+enum class StoragePolicy { e_heap, e_stack };
+
+DEEP_NAMESPACE_END
